b28b97007fe4aafca439d9b2c4a384ad927e4649
mickaelremilien <mickael.remilien@cloudwatt.com>
2016-07-05 11:21:09 +0200

    push mod3

--- images/bundle-trusty-wordpressHAweb/build.sh
+++ images/bundle-trusty-wordpressHAweb/build.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
+# bundle-build.sh BASENAME CW_BUNDLE_ID BUNDLE_PATH SRC_IMG IMG_OS
+
 IMG_UBUNTU_TRUSTY="ae3082cb-fac1-46b1-97aa-507aaa8f184f"
 SELF_PATH=`dirname "$0"`
 
 # bundle-build.sh BASENAME CW_BUNDLE_ID BUNDLE_PATH SRC_IMG IMG_OS
-bash "$SELF_PATH/../bundle-build.sh" "bundle-trusty-wordpress2" "WORDPRESS" "bundle-trusty-wordpress2" "$IMG_UBUNTU_TRUSTY" "Ubuntu"
+bash "$SELF_PATH/../build.sh" "bundle-trusty-wordpressHAweb" "VPN" "bundle-trusty-wordpressHAweb" "$IMG_UBUNTU_TRUSTY" "Ubuntu"
