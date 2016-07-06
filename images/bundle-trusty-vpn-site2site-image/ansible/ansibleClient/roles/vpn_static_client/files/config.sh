f72c762a94a13f495df34ff1d1cded60d4e07ef3
mickaelremilien <mickael.remilien@cloudwatt.com>
2016-07-06 14:33:19 +0200

    fix fin2

--- images/bundle-trusty-vpn-site2site-image/ansible/ansibleClient/roles/vpn_static_client/files/config.sh
+++ images/bundle-trusty-vpn-site2site-image/ansible/ansibleClient/roles/vpn_static_client/files/config.sh
@@ -1,7 +1,8 @@
 sudo tar xzvf  /etc/openvpn/configclient.tar.gz -C  /etc/openvpn/
 cd  /etc/openvpn/configclient
 sudo mv * ..
-sed -i 's/X.X.X.X/{{ip_server}}/g' /etc/openvpn/client.conf
+sed -i 's/X.X.X.X/ip_server/g' /etc/openvpn/client.conf
+sudo route del -net 10.10.10.0 netmask 255.255.255.0 gw 20.20.20.100
 sudo service openvpn restart
 sudo iptables -t nat -A POSTROUTING -o tun0 -j MASQUERADE
 sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
