15e56cac8cfcec510157ef0e9bde348e1840f214
mickael <mickael.remilien@orange.com>
2016-07-27 11:22:53 +0200

    dir

--- images/bundle-trusty-collectd-G-I-imagefr2/build.sh
+++ images/bundle-trusty-collectd-G-I-imagefr2/build.sh
@@ -6,4 +6,4 @@ IMG_UBUNTU_TRUSTY="ae3082cb-fac1-46b1-97aa-507aaa8f184f"
 SELF_PATH=`dirname "$0"`
 
 # bundle-build.sh BASENAME CW_BUNDLE_ID BUNDLE_PATH SRC_IMG IMG_OS
-bash "$SELF_PATH/../build.sh" "bundle-trusty-collectd-G-I-imagefr2" "VPN" "bundle-trusty-collectd-G-I-imagefr2" "$IMG_UBUNTU_TRUSTY" "Ubuntu"
+bash "$SELF_PATH/../build.sh" "bundle-trusty-collectd-G-I-imagefr2" "collectdGI" "bundle-trusty-collectd-G-I-imagefr2" "$IMG_UBUNTU_TRUSTY" "Ubuntu"
